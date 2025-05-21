b241f703a23701526aea38b898cac1fe3f1cd00e
lmoheyma <louismoheymani@gmail.com>
2025-05-21 23:13:32 +0200

    ⏳: Vagrantfile in server mode for Part2 -> Done

---
+++ part_2/k3s_server_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install K3S (server mode)
+echo "[SERVER] Deployment of K3S server..."
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=192.168.56.110" sh -
+
+echo "[SERVER] Serveur K3s deployed..."

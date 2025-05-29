d4921e762edd8d0c6c6c45ac47f78b4a2782fce5
lmoheyma <louismoheymani@gmail.com>
2025-05-29 20:13:15 +0200

    ✅ : Deployment of k3d cluster, ArgoCD namespace and port-forwarding

--- part_3/install_k3d.sh
+++ part_3/install_k3d.sh
@@ -3,10 +3,21 @@
 # Install k3d
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# Create cluser
+k3d cluster create --config /vagrant/config/k3d_cluster.yaml
+
 # Create first namespace (Argo CD)
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 # Create second namespace (dev)
-
 kubectl create namespace dev
+kubectl apply -n dev -f /vagrant/config/deployment.yaml
+
+# Port forward
+kubectl port-forward svc/argocd-server -n argocd 8080:443 &
+kubectl port-forward svc/argocd-server -n argocd 8080:443 &
